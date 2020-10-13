c60d0a2b4e5d64e7bcbeb8a71e0b0b7df576866b
Werawit Sirakunwat <werawit_boat@windowslive.com>
2020-10-13 16:46:50 +0700

    Update: edit uri mongoose to use env instead

--- server.js
+++ server.js
@@ -12,20 +12,18 @@ const userRoute = require('./routes/user');
 const registerRoute = require('./routes/register');
 const authRoute = require('./routes/auth');
 
+dotenv.config();
+
 const PORT = process.env.PORT || 8080;
-
-const uri = `mongodb://boat:boat@cluster0-shard-00-00.0mu0d.mongodb.net:27017,cluster0-shard-00-01.0mu0d.mongodb.net:27017,cluster0-shard-00-02.0mu0d.mongodb.net:27017/ISAG_Backend_HW?ssl=true&replicaSet=atlas-tjkeu0-shard-0&authSource=admin&retryWrites=true&w=majority`;
-
-mongoose.connect(uri, {
+ 
+mongoose.connect(process.env.URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
 
 
 require('./configs/passport');
-
-dotenv.config();
-
+ 
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
